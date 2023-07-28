266cad49db3b67211415bf4af874e741757080ee
bl4cks1d3 <blacknegroescuro@gmail.com>
2023-07-28 02:11:48 -0300

    inclui header e footer seguimentado

---
+++ app/footer.php
@@ -0,0 +1,63 @@
+<footer class="footer">
+        <div class="container">
+            <ul class="nav nav-pills nav-justified">
+                <li class="nav-item">
+                    <a class="nav-link" href="index">
+                        <span>
+                            <i class="nav-icon bi bi-house"></i>
+                            <span class="nav-text">Home</span>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="stats">
+                        <span>
+                            <i class="nav-icon bi bi-laptop"></i>
+                            <span class="nav-text">Statistics</span>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item centerbutton">
+                    <div class="nav-link">
+                        <span class="theme-radial-gradient">
+                            <i class="close bi bi-x"></i>
+                            <img src="../assets/img/centerbutton.svg" class="nav-icon" alt="">
+                        </span>
+                        <div class="nav-menu-popover justify-content-between">
+                            <button type="button" class="btn btn-lg btn-icon-text" onclick="window.location.replace('pay');">
+                                <i class="bi bi-credit-card size-32"></i><span>Pay</span>
+                            </button>
+
+                            <button type="button" class="btn btn-lg btn-icon-text" onclick="window.location.replace('sendmoney');">
+                                <i class="bi bi-arrow-up-right-circle size-32"></i><span>Send</span>
+                            </button>
+
+                            <button type="button" class="btn btn-lg btn-icon-text" onclick="window.location.replace('bills');">
+                                <i class="bi bi-receipt size-32"></i><span>Bills</span>
+                            </button>
+
+                            <button type="button" class="btn btn-lg btn-icon-text" onclick="window.location.replace('receivemoney');">
+                                <i class="bi bi-arrow-down-left-circle size-32"></i><span>Receive</span>
+                            </button>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="rewards">
+                        <span>
+                            <i class="nav-icon bi bi-gift"></i>
+                            <span class="nav-text">Rewards</span>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wallet">
+                        <span>
+                            <i class="nav-icon bi bi-wallet2"></i>
+                            <span class="nav-text">Wallet</span>
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
